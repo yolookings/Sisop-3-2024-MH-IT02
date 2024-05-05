5f8199d0eb8dc15c0940c35989009e3819fe2368
Maulana Ahmad Zahiri <maulanazahiri31@gmail.com>
2024-05-05 09:21:16 +0700

    edit project

--- soal_3/paddock.c
+++ soal_3/paddock.c
@@ -10,52 +10,6 @@
 #define PORT 8080
 #define LOG_FILE "race.log"
 
-// Fungsi untuk menentukan aksi berdasarkan jarak dengan musuh di depan
-char* Gap(float distance) {
-    if (distance < 3.5) {
-        return "Gogogo";
-    } else if (distance >= 3.5 && distance <= 10) {
-        return "Push";
-    } else {
-        return "Stay out of trouble";
-    }
-}
-
-// Fungsi untuk menentukan aksi berdasarkan sisa bensin
-char* Fuel(float fuel_percentage) {
-    if (fuel_percentage > 80) {
-        return "Push Push Push";
-    } else if (fuel_percentage >= 50 && fuel_percentage <= 80) {
-        return "You can go";
-    } else {
-        return "Conserve Fuel";
-    }
-}
-
-// Fungsi untuk menentukan aksi berdasarkan sisa pemakaian ban
-char* Tire(int tire_usage) {
-    if (tire_usage > 80) {
-        return "Go Push Go Push";
-    } else if (tire_usage >= 50 && tire_usage <= 80) {
-        return "Good Tire Wear";
-    } else if (tire_usage >= 30 && tire_usage < 50) {
-        return "Conserve Your Tire";
-    } else {
-        return "Box Box Box";
-    }
-}
-
-// Fungsi untuk menentukan aksi berdasarkan tipe ban saat ini
-char* TireChange(char* current_tire_type) {
-    if (strcmp(current_tire_type, "Soft") == 0) {
-        return "Mediums Ready";
-    } else if (strcmp(current_tire_type, "Medium") == 0) {
-        return "Box for Softs";
-    } else {
-        return "Invalid tire type";
-    }
-}
-
 // Fungsi untuk melakukan logging ke file race.log
 void log_message(char* source, char* command, char* additional_info) {
     time_t current_time;
